5d9335c1dd4abc732f098980c06df447b237e91f
guptamo <mo@mohitgupta.ca>
2017-03-31 17:19:21 -0400

    Created navbar and updated webpack config to use file-loader

--- source/index.js
+++ source/index.js
@@ -1,4 +1,26 @@
 import styles from './styles/app.css'
+import logo from './images/GoEastLogotypeWhite.svg'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 
-alert("hello")
+const Nav = ({style}) => {
+    return (
+        <nav className={style.nav}>
+            <div className={style.logo}></div>
+            <ul className={style.navList}>
+                <li>Pricing</li>
+                <li>Blog</li>
+                <li>Events</li>
+                <li>About Us</li>
+                <li>Sign In</li>
+            </ul>
+        </nav>
+    )
+}
+
+Nav.propTypes = {
+    style: React.PropTypes.object,
+}
+
+ReactDOM.render(<Nav style={styles}/>, document.querySelector('.container'))
